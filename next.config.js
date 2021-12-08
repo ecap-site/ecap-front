7f18e8ce659deedc357e0f4821cdee7b33ad30c5
snow-sr <joaofelipi877@gmail.com>
2021-12-08 08:23:25 -0300

    secret code

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     API_URL: 'https://ecap-back.herokuapp.com',
+    secret: 'L49RhZQxcfk3GlMybEQqoA=='
   },
   images:{
     domains: ['lh3.googleusercontent.com'] 
