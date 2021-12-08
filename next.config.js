05fbdef55caa1ddf769def0805feb1a4230ba686
snow-sr <joaofelipi877@gmail.com>
2021-12-08 08:45:27 -0300

    google oauth verify

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   env: {
     API_URL: 'https://ecap-back.herokuapp.com',
     SECRET: 'L49RhZQxcfk3GlMybEQqoA==',
-    GOOGLE_CLIENT_ID = "917430126285-bt1kgqks0m9shhu66pgv6738gkc55h7h.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET = "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"
+    GOOGLE_CLIENT_ID = '917430126285-bt1kgqks0m9shhu66pgv6738gkc55h7h.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET = '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'
   },
   images:{
     domains: ['lh3.googleusercontent.com'] 
